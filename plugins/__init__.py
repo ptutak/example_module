064e1b6cc2281702207b9780e1a6d0f616ea3c5a
Piotr Tutak <piotr.tutak@amartus.com>
2019-11-08 17:15:29 +0100

    testing

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,3 +7,15 @@ def hello():
 @plugin
 def upper(value: "string") -> "string":
     return value.upper()
+
+@plugin
+def is_employee(person: "Person") -> "bool":
+    if person.name == 'Piotr Tutak':
+        return True
+    return False
+
+@plugin
+def is_employer(person: "Person") -> "bool":
+    if person.name == "Przemyslaw Wawrzynczak":
+        return True
+    return False
