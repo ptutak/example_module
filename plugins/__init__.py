01a6eb787b255dac64330f28b21ee31bd257a81a
Piotr Tutak <piotr.tutak@amartus.com>
2019-12-09 10:35:09 +0100

    testing

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -67,11 +67,13 @@ class TestResourceHandler(CRUDHandler):
         raise ResourcePurged()
 
     def create_resource(self, context: HandlerContext, desired_resource: TestResource) -> None:
-        self._io.put(desired_resource.name, desired_resource.content.encode())
+        content = desired_resource.content
+        self._io.put(desired_resource.name, content.encode('utf-8'))
         context.set_created()
 
     def update_resource(self, context: HandlerContext, desired_resource: TestResource) -> None:
-        self._io.put(desired_resource.name, desired_resource.content.encode())
+        content = desired_resource.content
+        self._io.put(desired_resource.name, content.encode('utf-8'))
         context.set_updated()
 
     def delete_resource(self, context: HandlerContext, desired_resource: TestResource) -> None:
